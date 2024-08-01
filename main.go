de13ac723d11f876c5cec1bc56f3f303255fe598
AbtahiHasan <anikidpersonal@gmail.com>
2024-08-01 13:15:56 +0600

    update movie done

--- main.go
+++ main.go
@@ -63,6 +63,20 @@ func getMovie (w http.ResponseWriter, r *http.Request) {
 }
 
 
+func updateMovie (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.Id == params["id"] {
+			var movie Movie
+			movies = append(movies[:index], movies[index + 1:]...)
+			json.NewDecoder(r.Body).Decode(&movie)
+			movie.Id = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movies)
+		}
+	}
+}
 
 
 func main() {
